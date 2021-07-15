86489a55de673ff31cf9187df886d2152b965c41
= <=>
2021-07-15 02:39:34 -0700

    add request

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const router = Router()
-
 const routerController = require('../controllers/router.controller')
+
 router.get('/', routerController.home)
 router.get('/request', routerController.request)
 
